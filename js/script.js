4e8860f200f88a2a131081e0c788480db16f4b4e
Silvio Andres Rodriguez <jsilviojrodriguez@gmail.com>
2026-05-20 18:06:58 -0500

    Corrección final del menú y slider

--- js/script.js
+++ js/script.js
@@ -21,17 +21,41 @@ const hora = new Date().getHours();
 
 if (hora < 12) {
 
-    mensaje.innerHTML = " Buenos días, bienvenido al Boxeo Mexicano";
+    mensaje.innerHTML = "Buenos días, bienvenido al Boxeo Mexicano";
 
 }
 else if (hora < 18) {
 
-    mensaje.innerHTML = " Buenas tardes, disfruta la historia del Boxeo Mexicano";
+    mensaje.innerHTML = "Buenas tardes, disfruta la historia del Boxeo Mexicano";
 
 }
 else {
 
-    mensaje.innerHTML = " Buenas noches, conoce la cultura del Boxeo Mexicano";
+    mensaje.innerHTML = "Buenas noches, conoce la cultura del Boxeo Mexicano";
 }
 
 
+// SLIDER AUTOMÁTICO
+
+const imagenes = [
+    "images/Boxeo.jpg",
+    "images/Boxeo2.jpg",
+    "images/Boxeo3.jpg"
+];
+
+let indice = 0;
+
+const slider = document.getElementById("slider");
+
+setInterval(() => {
+
+    indice++;
+
+    if (indice >= imagenes.length) {
+
+        indice = 0;
+    }
+
+    slider.src = imagenes[indice];
+
+}, 3000);
