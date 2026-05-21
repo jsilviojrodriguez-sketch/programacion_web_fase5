254d252f013d0ab6c1f133f29de9326867fd0745
Silvio Andres Rodriguez <jsilviojrodriguez@gmail.com>
2026-05-21 17:19:25 -0500

    actualización del proyecto

--- js/script.js
+++ js/script.js
@@ -1,49 +1,73 @@
 const boton = document.getElementById("botonMenu");
 const menu = document.getElementById("menu");
 
+// MENÚ HAMBURGUESA
 boton.addEventListener("click", function () {
 
     if (menu.style.display === "flex") {
+
         menu.style.display = "none";
+
     } else {
+
         menu.style.display = "flex";
+
     }
 
 });
 
-// MENSAJE
+// MENSAJE DINÁMICO
 const mensaje = document.getElementById("mensaje");
-const hora = new Date().getHours();
 
-if (hora < 12) {
-    mensaje.innerHTML = "Buenos días, bienvenido al Boxeo Mexicano";
-}
-else if (hora < 18) {
-    mensaje.innerHTML = "Buenas tardes, disfruta la historia del Boxeo Mexicano";
-}
-else {
-    mensaje.innerHTML = "Buenas noches, conoce la cultura del Boxeo Mexicano";
-}
+if (mensaje) {
 
-// SLIDER
-const imagenes = [
-    "images/Boxeo.jpg",
-    "images/Boxeo2.jpg",
-    "images/Boxeo3.jpg"
-];
+    const hora = new Date().getHours();
 
-let indice = 0;
+    if (hora < 12) {
 
-const slider = document.getElementById("slider");
+        mensaje.innerHTML = "Buenos días, bienvenido al Boxeo Mexicano";
 
-setInterval(() => {
+    }
+    else if (hora < 18) {
 
-    indice++;
+        mensaje.innerHTML = "Buenas tardes, disfruta la historia del Boxeo Mexicano";
 
-    if (indice >= imagenes.length) {
-        indice = 0;
+    }
+    else {
+
+        mensaje.innerHTML = "Buenas noches, conoce la cultura del Boxeo Mexicano";
+
     }
 
-    slider.src = imagenes[indice];
+}
 
-}, 3000);
+// SLIDER SOLO SI EXISTE
+const slider = document.getElementById("slider");
+
+if (slider) {
+
+    const imagenes = [
+
+        "images/Boxeo.jpg",
+        "images/Boxeo2.jpg",
+        "images/Boxeo3.jpg"
+
+    ];
+
+    let indice = 0;
+
+    setInterval(() => {
+
+        indice++;
+
+        if (indice >= imagenes.length) {
+
+            indice = 0;
+
+        }
+
+        slider.src = imagenes[indice];
+
+    }, 3000);
+
+}
